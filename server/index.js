386376d0cf918e69e4df1cd2386e2e141661d3e7
alexmassimino9 <alexmassimino9@gmail.com>
2023-04-04 22:28:53 -0400

    add gitignore and connection to mongo

--- server/index.js
+++ server/index.js
@@ -20,3 +20,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // MongoDB connection
+
+const PORT = process.env.PORT || 9000;
+
+mongoose
+  .connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log("Connected to MongoDB")
+  )
+  .then(async () => {
+    app
+      .listen(PORT, () => {
+        console.log(`Server is running on port ${PORT} `);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to the database");
+});
