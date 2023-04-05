582aa925d38a41e6ec118e5bc16a7f23867a05e3
alexmassimino9 <alexmassimino9@gmail.com>
2023-04-05 05:27:15 -0400

    add types

--- client/src/views/dashboard/Row1.tsx
+++ client/src/views/dashboard/Row1.tsx
@@ -13,15 +13,23 @@ const Row1 = (props: Props) => {
 
     const { data } = useGetKpisQuery()
     console.log("data", data)
+    const revenueExpenses = useMemo(() => {
+      return (
+        data &&
+        data[0].monthlyData.map(({ month, revenue, expenses })=>{
+          return { name: month.substring(0,3), revenue: revenue, expenses:expenses }
+        })
+      )
+    } , [])
 
-
+    console.log("revenueExpenses", revenueExpenses)
     return (
        <>
-       <DashboardBox gridArea="a">
-       <ResponsiveContainer width="100%" height="100%">
+         <ResponsiveContainer width="100%" height="100%">
         <AreaChart
           width={500}
           height={400}
+          data={data}
           margin={{
             top: 10,
             right: 30,
@@ -35,10 +43,7 @@ const Row1 = (props: Props) => {
           <Tooltip />
           <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
         </AreaChart>
-      </ResponsiveContainer> 
-       </DashboardBox> 
-       <DashboardBox gridArea="b"/> 
-       <DashboardBox gridArea="c"/> 
+      </ResponsiveContainer>
        
        </>
 
