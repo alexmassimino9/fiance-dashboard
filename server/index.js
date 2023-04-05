ecdb99e3726bb3ae783af76445bcaac47d7b8029
alexmassimino9 <alexmassimino9@gmail.com>
2023-04-04 21:23:13 -0400

    fix

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import morgan from "morgan";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import morgan from "morgan";
 import helmet from "helmet";
+
+//  configurations
+
+dotenv.config();
 const app = express();
+
+app.use(express.json());
+app.use(helmet());
