7e26be425be64dd7a430a4d51010b34d46d1a23c
alexmassimino9 <alexmassimino9@gmail.com>
2023-04-04 21:41:50 -0400

    server configurations

--- server/index.js
+++ server/index.js
@@ -13,3 +13,10 @@ const app = express();
 
 app.use(express.json());
 app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// MongoDB connection
