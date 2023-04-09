dc94f9fa81e59f7f367695067b6164b39e223add
alexmassimino9 <alexmassimino9@gmail.com>
2023-04-09 13:48:03 -0400

    add monthly revenue widget

--- client/src/views/dashboard/Row1.tsx
+++ client/src/views/dashboard/Row1.tsx
@@ -2,16 +2,29 @@ import DashboardBox from '@/components/DashboardBox'
 import { useGetKpisQuery } from '@/state/api'
 import { useTheme } from '@mui/material'
 import { useMemo } from 'react'
-import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis, Line, Legend, LineChart } from 'recharts'
+import { Area, 
+  BarChart, Bar,
+  AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis, Line, Legend, LineChart } from 'recharts'
 import BoxHeader from '@/components/BoxHeader'
 
 
 interface Props {}
 
-const Row1 = (props: Props) => {
+const Row1 = () => {
     const { palette } = useTheme();
     const { data } = useGetKpisQuery()
 
+    const revenue = useMemo(() => {
+      return (
+        data &&
+        data[0].monthlyData.map(({ month, revenue,  })=>{
+          return { 
+            name: month.substring(0,3), 
+            revenue:revenue, 
+          }
+        })
+      )
+    } , [data])
     const revenueExpenses = useMemo(() => {
       return (
         data &&
@@ -19,7 +32,7 @@ const Row1 = (props: Props) => {
           return { 
             name: month.substring(0,3), 
             revenue:revenue, 
-            expenses: expenses 
+            expenses: expenses.toFixed(2)
           }
         })
       )
@@ -99,6 +112,7 @@ const Row1 = (props: Props) => {
         </AreaChart>
       </ResponsiveContainer>
        </DashboardBox>
+
       <DashboardBox gridArea="b">
       <BoxHeader title="Profit and Revenue" 
           subtitle='top line represents revenue and bottom line represents expenses'
@@ -154,8 +168,49 @@ const Row1 = (props: Props) => {
       </ResponsiveContainer>
       </DashboardBox>
 
+      <DashboardBox gridArea={"c"}> 
+          <BoxHeader 
+          title="Revenue Month by Month" 
+          subtitle='graph representing revenue month by month'
+          sideText="+4%" 
+          />
+  <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={500}
+          height={300}
+          data={revenue}
+          margin={{
+            top: 17,
+            right: 15,
+            left: -5,
+            bottom: 58,
+          }}
+        >
+           <defs>
+            <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.5} />
+              <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <CartesianGrid vertical={false} strokeDasharray="3 3" />
+          <XAxis 
+           dataKey="name"
+           axisLine={false}
+           tickLine={false}
+           style={{fontSize: "10px"}}
+           />
+          <YAxis
+          axisLine={false}
+          tickLine={false}
+          style={{fontSize: "10px"}}
+          />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="revenue" fill="url(#colorRevenue)" />
+        </BarChart>
+      </ResponsiveContainer>
 
-      <DashboardBox gridArea={"c"}> </DashboardBox>
+      </DashboardBox>
    
        
        </>
